51ea4b4dc1103154d93838300b252b2cf11b017d
IuliiaKalenyk <yulyaa.kalenyk@gmail.com>
2021-07-06 13:13:18 +0300

    finished task

--- src/components/Feedback/Feedback.styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-
-export const Container = styled.div``;
-export const FeedbackTitle = styled.h1``;
-export const FeedbackMarks = styled.div``;
-export const FeedbackButton = styled.button``;
-export const FeedbackStatistics = styled.h2``;
-export const FeedbackList = styled.ul``;
-export const FeedbackItem = styled.li``;
